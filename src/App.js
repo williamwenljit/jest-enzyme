be4a144e51b310e575820f167a39c4554d74bfa3
William Wen <william-wen@ljit.io>
2019-03-08 15:31:47 +0800

    Update env

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
+import TodoList from './components/todo-list';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          React Test Demo
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			list: [],
+			inputValue: '',
+		};
+		this._handleInputChange = this._handleInputChange.bind(this);
+		this._handleItemAdd = this._handleItemAdd.bind(this);
+		this._handleItemRemove = this._handleItemRemove.bind(this);
+	}
+
+	_handleInputChange(event) {
+		this.setState({ inputValue: event.target.value });
+	}
+
+	_handleItemAdd(item) {
+		const list = this.state.list.concat([item]);
+		this.setState({ list, inputValue: '' });
+	}
+
+	_handleItemRemove(index) {
+		const list = this.state.list.slice();
+		list.splice(index, 1);
+		this.setState({ list });
+	}
+
+	render() {
+		const {
+			list,
+			inputValue,
+		} = this.state;
+
+		return (
+			<div className="App">
+				<header className="app-header">
+					React Test Demo
+				</header>
+				<div>
+					<TodoList
+						title="Todo list 1"
+						list={list}
+						inputValue={inputValue}
+						onInputChange={this._handleInputChange}
+						onItemAdd={this._handleItemAdd}
+						onItemRemove={this._handleItemRemove}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
